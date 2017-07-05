b5f8c80609ef7f2dd2c037ac1147f6ac4cfcc9f8
guilima <gui.lima30@gmail.com>
2017-07-05 18:32:24 -0300

    Add HRM config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
 const webpack = require('webpack');
 const path = require('path');
 
+var proxyURL = 'http://local.financial.indexs'; // Your external HTML server
+var proxy = {
+  '*': { target: proxyURL }
+};
+
 module.exports = {
   context: path.resolve(__dirname, './src'),
   entry: {
     app: './app.js',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin() // Enable HMR
+  ],
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bundle.js',
   },
   resolve: {
     alias: { vue: 'vue/dist/vue.esm.js' }
+  },
+  devServer: {
+
+    open: true,
+    publicPath: "/dist/",
+    hot: true, // Tell the dev-server we're using HMW
+    //https: true,
+    host: 'local.financial.index',
+    port: 8082,
+    openPage: '',
   }
 };
