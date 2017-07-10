38c26dee615cd75f8bba0f169f542e083d238a74
guilima <gui.lima30@gmail.com>
2017-07-10 18:54:08 -0300

    Resolve modules absolute path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: "/dist/",
+    publicPath: '/dist/',
     filename: 'app.bundle.js'
   },
   module: {
@@ -27,6 +27,7 @@ module.exports = {
     ]
   },
   resolve: {
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
     alias: { vue: 'vue/dist/vue.esm.js' }
   },
   devServer: {
