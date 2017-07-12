84b140e88152ff68aecfbbc23d56130f96649aac
guilima <gui.lima30@gmail.com>
2017-07-12 17:34:32 -0300

    Remove class from template

--- src/components/filter/filter-date.vue
+++ src/components/filter/filter-date.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="ui labeled input">
     <label class="ui label" for="dateInitial">Inicial</label>
     <input class="mr-10" type="text" id="dateInitial" value="" v-model="dateInitial" placeholder="MM/YYYY" v-on:keyup="inputDateInitial(dateInitial, $event)">
     <label for="dateEnd" class="ui label">Final</label>
@@ -16,7 +16,7 @@ export default {
   methods: {
     inputDateInitial: function (date, event) {
       if (event.key !== "Backspace") {
-        if (date.match(/^(?![0-1])/)) this.dateInitial = "";
+        if (date.match(/^(?![0-1])/)) console.log(date);
         if (date.match(/^(1[0-2]|0[1-9])/)) this.dateInitial = date.substring(0, 2) + "/" + date.substring(3, 7);
       }
       this.groupDate;
