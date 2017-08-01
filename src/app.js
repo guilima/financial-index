ee32032a54ec0fcc1c6087e06219dfdf41443858
guilima <gui.lima30@gmail.com>
2017-07-31 23:26:24 -0300

    Add chartist component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import AppFilter from 'components/filter/app-filter.vue';
+import Chartist from 'chartist';
 import valuesService from 'services/valueSeries';
 import Vue from 'vue';
 
@@ -61,6 +62,29 @@ function seriesByDate(series, ibovespaLastMonth) {
   return reOrders;
 }
 
+function chartist(series) {
+  var dates = series.map(serie => serie[0]);
+  // var chartSeries = [];
+  // series.forEach((serie, i) => {
+  //   chartSeries[i] = [];
+  //   serie.forEach((value,index) => {
+  //     chartSeries[i].push(serie[index]);
+  //   });
+  // });
+  // console.log(chartSeries);
+  var data = {
+    // A labels array that can contain any sort of values
+    labels: dates,
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      [12, 9, 7, 8, 5, 5],
+      [2, 1, 3.5, 7, 3, 7],
+      [1, 3, 4, 5, 6, 2]
+    ]
+  };
+  new Chartist.Line('.ct-chart', data);
+}
+
 function valore(values) {
   var dataService = JSON.parse(values),
       ibovespaLastMonth = dataService[1],
@@ -79,4 +103,5 @@ function valore(values) {
     }
   });
   vm.series = seriesByDate(series, ibovespaLastMonth);
+  chartist(vm.series);
 }
