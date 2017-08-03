d50f00b2ae3e7bee7f6b6194c990528faf115a07
guilima <gui.lima30@gmail.com>
2017-08-03 17:51:40 -0300

    Add echarts createFunction and options

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import AppFilter from 'components/filter/app-filter.vue';
 import valuesService from 'services/valueSeries';
 import Vue from 'vue';
+import echarts from 'echarts/dist/echarts.common.js';
 
 var vm = new Vue({
   el: '#app-4',
@@ -61,26 +62,83 @@ function seriesByDate(series, ibovespaLastMonth) {
   return reOrders;
 }
 
-function createChart(series) {
+function createChart(series, names) {
   var dates = series.map(serie => serie[0]);
   var chartSeries = [];
+  var copyNames = Vue.util.extend([], names); //ignore dates
+  copyNames.unshift('dates');
+  console.log(names);
   series[0].forEach((serie, i) => {
-    chartSeries[i] = [];
+      console.log(chartSeries);
+    chartSeries[i] = {
+      name: copyNames[i],
+      areaStyle: {normal: {}},
+      type: 'line',
+      data: []
+    };
   });
   series.forEach((serie, i) => {
     serie.forEach((value,index) => {
       if(serie[index] === '-') {
-        chartSeries[index].push( null );
+        chartSeries[index].data.push( null );
       } else {
-        chartSeries[index].push( Number( serie[index].replace('%','') ) );
+        chartSeries[index].data.push( Number( serie[index].replace('%','') ) );
       }
     });
   });
   chartSeries.shift();
-  var data = {
-    labels: dates,
+  // based on prepared DOM, initialize echarts instance
+  var myChart = echarts.init(document.getElementById('chart'));
+  window.onresize = function() {
+    myChart.resize();
+  };
+  // specify chart configuration item and data
+  var option = {
+    title: {
+      text: ''
+    },
+    tooltip: {},
+    legend: {
+      data: chartSeries
+    },
+    xAxis: [{
+      type : 'category',
+      boundaryGap : false,
+      data : dates
+    }],
+    yAxis : [{
+      type : 'value',
+      min: 'dataMin',
+      max: 'dataMax',
+      axisLabel: {
+        formatter: '{value}%'
+      }
+    }],
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 10
+      },
+      {
+        start: 0,
+        end: 10,
+        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+        handleSize: '80%',
+        handleStyle: {
+          color: '#fff',
+          shadowBlur: 3,
+          shadowColor: 'rgba(0, 0, 0, 0.6)',
+          shadowOffsetX: 2,
+          shadowOffsetY: 2
+        }
+      }
+    ],
     series: chartSeries
   };
+
+  // use configuration item and data specified to show chart
+  myChart.setOption(option);
 }
 
 function valore(values) {
@@ -101,5 +159,5 @@ function valore(values) {
     }
   });
   vm.series = seriesByDate(series, ibovespaLastMonth);
-  chartist(vm.series);
+  createChart(vm.series, vm.names);
 }
