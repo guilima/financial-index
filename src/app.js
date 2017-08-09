9dca76d0723cafe0dea210b0bc78154ac62b49aa
guilima <gui.lima30@gmail.com>
2017-08-08 23:01:26 -0300

    Adjust echarts reverse, tooltip and dataZoom.Remove areaStyle option

--- src/app.js
+++ src/app.js
@@ -72,7 +72,6 @@ function createChart(series, names) {
       console.log(chartSeries);
     chartSeries[i] = {
       name: copyNames[i],
-      areaStyle: {normal: {}},
       type: 'line',
       data: []
     };
@@ -97,13 +96,23 @@ function createChart(series, names) {
     title: {
       text: ''
     },
-    tooltip: {},
+    tooltip: {
+        trigger: 'axis',
+        formatter: function (params, ticket, callback) {
+          let formattedText = params.map(param => {
+            if(param.value === undefined) param.value = '-';
+            return `<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:${param.color};"></span>${param.seriesName}: ${param.value}%`;
+          });
+          return `${params[0].name}<br>${formattedText.join('<br>')}`;
+        }
+    },
     legend: {
       data: chartSeries
     },
     xAxis: [{
       type : 'category',
       boundaryGap : false,
+      inverse:  true,
       data : dates
     }],
     yAxis : [{
@@ -118,11 +127,11 @@ function createChart(series, names) {
       {
         type: 'inside',
         start: 0,
-        end: 10
+        end: 100
       },
       {
         start: 0,
-        end: 10,
+        end: 100,
         handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
         handleSize: '80%',
         handleStyle: {
