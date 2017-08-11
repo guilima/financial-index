aa75f5fedde7123ed8f429c47f80ae5d5fdc6b7b
guilima <gui.lima30@gmail.com>
2017-08-11 16:30:34 -0300

    Adjust chart white-spaces and bg color

--- src/app.js
+++ src/app.js
@@ -99,8 +99,9 @@ function createChart(series, names) {
     },
     color: ['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2'],
     grid: {
+      height: '70%',
       show: true,
-      backgroundColor: 'rgba(128, 128, 128, 0.2)'
+      backgroundColor: 'rgba(255, 255, 255, 0.2)'
     },
     tooltip: {
         trigger: 'axis',
@@ -119,14 +120,14 @@ function createChart(series, names) {
       type : 'category',
       name: 'Datas',
       nameLocation: 'middle',
-      nameGap: 45,
+      nameGap: 30,
       boundaryGap : false,
       inverse:  true,
       data : dates
     }],
     yAxis : [{
       type : 'value',
-      name: 'Índices',
+      name: 'Variações dos Índices',
       nameGap: 45,
       nameLocation: 'middle',
       min: 'dataMin',
@@ -142,6 +143,7 @@ function createChart(series, names) {
         end: 100
       },
       {
+        type: 'slider',
         start: 0,
         end: 100,
         handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
