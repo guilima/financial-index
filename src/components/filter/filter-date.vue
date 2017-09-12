8a1eef479cf0d94e6e3eec387bec0a48ff6e36b3
guilima <gui.lima30@gmail.com>
2017-09-12 01:02:52 -0300

    Adjust filter date

--- src/components/filter/filter-date.vue
+++ src/components/filter/filter-date.vue
@@ -1,38 +1,29 @@
 <template>
   <div class="ui labeled input">
-    <label for="dateEnd" class="ui label" v-if="label" v-text="label"></label>
-    <input class="mr-10" type="text" id="dateInitial"
+    <label :for="label" class="ui label" v-if="label" v-text="label"></label>
+    <input class="mr-10" type="text" :id="label"
       ref="input"
       :value="date"
       placeholder="MM/YYYY"
-      @keyup="inputDate($event.target.value, $event)">
+      @keyup="updateDate($event.target.value, $event)">
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    date: {
-      type: String,
-      default: ''
-    },
-    label: {
-      type: String,
-      default: ''
-    }
-  },
+  props: ['date', 'label'],
   methods: {
-    inputDate: function (date, event) {
+    updateDate: function (date, event) {
+      var formattedDate;
       if (event.key !== "Backspace") {
-        var itDate = date;
-        if (date.match(/^(?!1[0-2])/)) itDate = date.substring(0, 1);
-        if (date.match(/^(?![0-1])/)) itDate = '';
-        if (date.match(/^(0[1-9]|1[0-2])/)) itDate = date.substring(0, 2) + "/" + date.substring(3, 7);
-
-        if(itDate !== date)
-          this.$refs.input.value = itDate;
+        if (date.match(/^(?!1[0-2])/)) formattedDate = date.substring(0, 1);
+        if (date.match(/^(?![0-1])/)) formattedDate = '';
+        if (date.match(/^(0[1-9]|1[0-2])/)) formattedDate = date.substring(0, 2) + "/" + date.substring(3, 7);
       }
-      this.$emit('input', itDate);
+      if(formattedDate !== date)
+        this.$refs.input.value = formattedDate;
+
+      this.$emit('input', formattedDate);
     }
   }
 };
