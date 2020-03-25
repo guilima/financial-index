14fad9b98add878909d1a61bdcb19ff2486d29f8
Guilherme Lima <gui.lima30@gmail.com>
2020-03-25 16:24:20 -0300

    refactor(valueSeries): Set environment variable for backend API

--- src/services/valueSeries.js
+++ src/services/valueSeries.js
@@ -2,7 +2,7 @@ function getValuesSeriesService(dateInitial = '', dateEnd = '', series = '') {
   return new Promise(function (resolve, reject) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', `http://localhost:1337/series?dateInitial=${dateInitial}&dateEnd=${dateEnd}&idGroup=${series}`);
+    xhr.open('GET', `${process.env.VUE_APP_API}/series?dateInitial=${dateInitial}&dateEnd=${dateEnd}&idGroup=${series}`);
     xhr.setRequestHeader('content-type', 'application/json');
     xhr.withCredentials = false;
 
